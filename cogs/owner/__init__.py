47acb1d9f7a2811af8b33f1b0eb18ffbcd0fddc6
galind <guillem.yt1@gmail.com>
2022-10-29 04:26:53 +0200

    Minor fix

--- cogs/owner/__init__.py
+++ cogs/owner/__init__.py
@@ -1,6 +1,6 @@
-from core.bot import Economy
+from core.bot import Playground
 from .owner import Owner
 
 
-async def setup(bot: Economy):
+async def setup(bot: Playground):
     await bot.add_cog(Owner(bot))
