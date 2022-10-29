be173b1fcd971f89071660bcd11221222c85f8b7
galind <guillem.yt1@gmail.com>
2022-10-29 13:46:58 +0200

    Added function to calculate the value of a hand

--- cogs/blackjack/blackjack.py
+++ cogs/blackjack/blackjack.py
@@ -33,14 +33,35 @@ def get_deck():
     for s in suits:
         for c in cards:
             deck.append(f'{c}{s}')
+    return shuffle_deck(deck)
+
+
+def shuffle_deck(deck: list):
+    random.shuffle(deck)
+    random.shuffle(deck)
+    random.shuffle(deck)
     return deck
 
 
-def suffle_deck(deck):
-    random.shuffle(deck)
-    random.shuffle(deck)
-    random.shuffle(deck)
-    return deck
+def calculate_hand(hand: list):
+    aces = [c for c in hand if c[0] == 'A']
+    non_aces = [c for c in hand if c[0] != 'A']
+
+    hand_sum = 0
+    for c in non_aces:
+        hand_sum += cards[c[0]]
+    for c in aces:
+        if hand_sum <= 10:
+            hand_sum += 11
+        else:
+            hand_sum += 1
+    return hand_sum
+
+
+class GameButtons(discord.ui.View):
+    def __init__(self, bot: Playground):
+        self.bot = bot
+        super().__init__(timeout=None)
 
 
 class Blackjack(commands.Cog):
